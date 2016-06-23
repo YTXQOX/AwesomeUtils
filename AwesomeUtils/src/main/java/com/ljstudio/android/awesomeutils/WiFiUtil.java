ba59bec7a6bbef0cec151684b14baae5413313c4
ljstudio <ljrfoxteenstitan@gmail.com>
2016-06-23 10:41:41 +0800

    update

--- AwesomeUtils/src/main/java/com/ljstudio/android/awesomeutils/WiFiUtil.java
+++ AwesomeUtils/src/main/java/com/ljstudio/android/awesomeutils/WiFiUtil.java
@@ -47,7 +47,7 @@ public class WiFiUtil {
         return mac;
     }
 
-    public static String intToIp(int i) {
+    private static String intToIp(int i) {
         return (i & 0xFF) +
                 "." + ((i >> 8) & 0xFF) +
                 "." + ((i >> 16) & 0xFF) +
